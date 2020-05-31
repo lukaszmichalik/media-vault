0d9940bfb373db3160789d0fad19cdd067bc3f96
Lukasz Michalik <lukaszmichalik98@gmail.com>
2020-05-31 15:50:06 +0200

    zmiana widoków

--- app/src/main/java/com/example/firebaseapp/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/firebaseapp/adapter/MovieAdapter.kt
@@ -44,9 +44,6 @@ class MovieAdapter(val mCtx:Context,val layoutResId:Int,val movieList:List<Movie
     }
 
     private fun deleteMovie(movie: Movie) {
-        /*val builder = AlertDialog.Builder(mCtx)
-        builder.setTitle("Delete Movie")
-        val dbMovie = FirebaseDatabase.getInstance().getReference("movies")*/
 
         val name:String=movie.name
         val alertDialog = AlertDialog.Builder(context)
@@ -55,7 +52,7 @@ class MovieAdapter(val mCtx:Context,val layoutResId:Int,val movieList:List<Movie
             .setPositiveButton("Yes") { dialog, which ->
                 val dbMovie = FirebaseDatabase.getInstance().getReference("movies").child(movie.id)
                 dbMovie.removeValue()
-                    Toast.makeText(context,"Product $movie.name Deleted", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context,"Product $name Deleted", Toast.LENGTH_SHORT).show()
                 }
             .setNegativeButton("No", DialogInterface.OnClickListener { dialog, which ->  })
             .show()
@@ -63,48 +60,36 @@ class MovieAdapter(val mCtx:Context,val layoutResId:Int,val movieList:List<Movie
     }
 
     private fun showUpdateDialog(movie: Movie) {
+
         val builder = AlertDialog.Builder(mCtx)
+
         builder.setTitle("Update Movie")
+
         val inflater = LayoutInflater.from(mCtx)
 
         val view = inflater.inflate(R.layout.layout_update_movie, null)
 
-        //val editText = view.findViewById<>(R.id.editTextUpdate)
         val editText= view.findViewById<TextInputEditText>(R.id.editTextUpdate)
         val ratingBar = view.findViewById<RatingBar>(R.id.ratingBar)
         val textInputLayout = view.findViewById(R.id.text_input_layout) as TextInputLayout
 
-        /*editText.setText(movie.name)*/
         editText.setText(movie.name)
-        /*val inputText = editText.editText?.text.toString()*/
         ratingBar.rating = movie.rating.toFloat()
 
         builder.setView(view)
 
         builder.setPositiveButton("Update") { p0, p1 ->
-            val dbMovie = FirebaseDatabase.getInstance().getReference("movies")
-            /*val name = editText.text.toString().trim()*/
-            val name = editText.text.toString().trim()
-
-            if(name.isEmpty()){
-                textInputLayout.error="Please enter name"
-                editText.requestFocus()
-            }
-            val movie2 = Movie(movie.id, name, ratingBar.rating.toInt())
-
-            dbMovie.child(movie.id).setValue(movie2)
-
-            Toast.makeText(mCtx,"Movie updated", Toast.LENGTH_LONG).show()
         }
 
         builder.setNegativeButton("No") { p0, p1 ->
         }
+
          val dialog = builder.create()
          dialog.show()
         dialog.getButton(AlertDialog.BUTTON_POSITIVE)
             .setOnClickListener {
+
                 val dbMovie = FirebaseDatabase.getInstance().getReference("movies")
-                /*val name = editText.text.toString().trim()*/
                 val name = editText.text.toString().trim()
 
                 if(name.isEmpty()){
@@ -117,55 +102,11 @@ class MovieAdapter(val mCtx:Context,val layoutResId:Int,val movieList:List<Movie
 
                     Toast.makeText(mCtx, "Movie updated", Toast.LENGTH_LONG).show()
                     Log.d("tag3: ", "dziala")
-                    val wantToCloseDialog = false
-                    //Do stuff, possibly set wantToCloseDialog to true then...
-
-                    // showToast
 
                     dialog.dismiss()
-                    // other stuff to do
+
                 }
-                //else dialog stays open. Make sure you have an obvious way to close the dialog especially if you set cancellable to false.
+
             }
     }
 }
-
-
-
-/*class MovieAdapter (val mCtx:Context,val layoutResId:Int,val movieList:MutableList<Movie>)
-    : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.movies, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun getItemCount(): Int {
-        return movieList.size
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        var movie=movieList[position]
-        holder.movieName.setText(holder.adapterPosition)
-        holder.movieRatingBar.rating= movie.rating.toFloat()
-    }*/
-
-  /*  override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val layoutInflater: LayoutInflater = LayoutInflater.from(mCtx)
-        val view: View = layoutInflater.inflate(layoutResId, null)
-
-        val textViewName = view.findViewById<TextView>(R.id.textView)
-
-        val movie = movieList[position]
-
-        textViewName.text = movie.name
-        return view
-    }*/
-
-    /*class ViewHolder(view: View): RecyclerView.ViewHolder(view){
-
-        val movieView=view
-        var movieName=view.findViewById<EditText>(R.id.textViewName)
-        var movieRatingBar=view.findViewById<RatingBar>(R.id.ratingBar)
-        //var deleteButton=view.findViewById<Button>(R.id.deleteButton)
-    }}
-*/
--- app/src/main/java/com/example/firebaseapp/ui/movies/MovieFragment.kt
+++ app/src/main/java/com/example/firebaseapp/ui/movies/MovieFragment.kt
@@ -14,14 +14,10 @@ import com.google.firebase.database.*
 class MovieFragment : Fragment() {
 
     private lateinit var movieViewModel: MovieViewModel
-
-    lateinit var  editText: EditText
-    lateinit var  ratingBar: RatingBar
-    lateinit var button: Button
-
-    lateinit var movieList: MutableList<Movie>
     lateinit var ref: DatabaseReference
+    lateinit var movieList: MutableList<Movie>
     lateinit var listView: ListView
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,21 +29,15 @@ class MovieFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_movie, container, false)
         setHasOptionsMenu(true)
         movieList = mutableListOf()
-        ref = FirebaseDatabase.getInstance().getReference("movies")
-
-        editText= root.findViewById(R.id.editText)
-        ratingBar= root.findViewById(R.id.ratingBar)
-        button= root.findViewById(R.id.button)
         listView= root.findViewById(R.id.listView)
-        button.setOnClickListener {
-            saveMovie()
-        }
+        ref = FirebaseDatabase.getInstance().getReference("movies")
         ref.addValueEventListener(object: ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
                 TODO("Not yet implemented")
             }
 
             override fun onDataChange(p0: DataSnapshot) {
+                movieList.clear()
                 if(p0.exists()){
                     movieList.clear()
                     for (h in p0.children){
@@ -55,15 +45,11 @@ class MovieFragment : Fragment() {
                         movieList.add(movie!!)
                     }
 
-                    /*root.list_of_products.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL,false)
-                    list_of_products.adapter=MovieAdapter(context!!, R.layout.movies, movieList)*/
-                   /* val adapter = MovieAdapter(context!!,R.layout.movies, movieList)
-
-                    listView.layoutManger = adapter*/
                     val context = context
                     if (context !=null){
-                    val adapter = MovieAdapter(context, R.layout.movies, movieList)
-                    listView.adapter = adapter}
+                        val adapter = MovieAdapter(context, R.layout.movies, movieList)
+                        listView.adapter = adapter}
+
                 }
             }
         })
@@ -73,24 +59,6 @@ class MovieFragment : Fragment() {
         return root
     }
 
-    private fun saveMovie() {
-        val name = editText.text.toString().trim()
-
-        if(name.isEmpty()){
-            editText.error = "Please enter a name"
-            return
-        }
-
-        val ref = FirebaseDatabase.getInstance().getReference("movies")
-        val movieId = ref.push().key
-        val movie = movieId?.let { Movie(it,name, ratingBar.rating.toInt()) }
-
-        if (movieId != null) {
-            ref.child(movieId).setValue(movie).addOnCompleteListener {
-               // Toast.makeText(context, "movie saved successfully", Toast.LENGTH_LONG).show()
-            }
-        }
-    }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.add_menu,menu)
